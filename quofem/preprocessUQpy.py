3618e676d3dae60dfd2354a32a577175ac1e117b
Dimitris Tsapetis <dimtsap@hotmail.com>
2023-03-15 12:08:44 -0400

    Populates preprocessUQpy.py

--- quofem/preprocessUQpy.py
+++ quofem/preprocessUQpy.py
@@ -1,6 +1,9 @@
 import click
 import os
 
+from src.quofemDTOs import Model
+from src.runmodel.RunModelDTOs import RunModelDTO
+
 
 @click.command()
 @click.option('--workflowInput', type=click.Path(exists=True, readable=True), required=True,
@@ -12,7 +15,29 @@ import os
 @click.option('--osType', type=click.Choice(['Linux', 'Windows']),
               help="Type of operating system the workflow will run on.")
 def preprocess(workflowinput, driverfile, runtype, ostype):
-    pass
+    code = []
+
+    with open(os.path.join(os.getcwd(), workflowinput)) as my_file:
+        json = my_file.read()
+        model = Model.parse_raw(json)
+
+    marginals_code = 'marginals = ['
+    for distribution in model.randomVariables:
+        (distribution_code, input) = distribution.init_to_text()
+        marginals_code += input + ','
+        code.append(distribution_code)
+    marginals_code += ']\n'
+    code.append(marginals_code)
+
+    runmodel_code = RunModelDTO.create_runmodel_with_variables_driver(variables=model.randomVariables,
+                                                                      driver_filename="driver")
+    (uqmethod_code, _) = model.UQ.methodData.generate_code()
+
+    code.append(runmodel_code)
+    code.append(uqmethod_code)
+
+    with open("UQpyAnalysis.py", 'w') as outfile:
+        outfile.write("\n".join(code))
 
 
 if __name__ == "__main__":
